8ce8e79d7a8d76800b7d95ac83d559565d367cd6
onceAuser <55274251+onceAuser@users.noreply.github.com>
2019-10-13 10:49:33 +0530

    Update anyfactorial.py

--- anyfactorial.py
+++ anyfactorial.py
@@ -1,5 +1,5 @@
-def facto(x):
-    fac
+def fact(x):
+    facto=[None]*(n+1)
     if x<=1:
         return 1
     if facto[x]!=None:
@@ -7,4 +7,4 @@ def facto(x):
     for i in range(1,x+1):
         facto[i]=facto[i-1]*i
     return facto[x]
-print(facto(1000))
+print(fact(1000))
